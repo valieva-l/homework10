1bedbd8b8a09f0a84a247c2920665eaeaf0c2c76
lyays <l28075339@gmail.com>
2025-03-27 22:48:22 +0300

    hm10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Задача 1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("Задача 2");
+        String fullNameOne = "Ivanov Ivan Ivanovich";
+        String fullNameTwo = fullNameOne.toUpperCase();
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullNameTwo);
+
+        System.out.println("Задача 3");
+        String fullNameThree = "Иванов Семён Семёнович";
+        String fullNameFour = fullNameThree.replace('ё', 'е');
+
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameFour);
     }
 }
